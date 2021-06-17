8d90ad0e8555c74724074ec8a67a74f7899adfea
forgedhallpass <istvan@projectdiscovery.io>
2021-06-17 17:20:53 +0300

    Update documentation

--- src/main/java/io/projectdiscovery/plugins/jenkins/nuclei/NucleiDownloader.java
+++ src/main/java/io/projectdiscovery/plugins/jenkins/nuclei/NucleiDownloader.java
@@ -3,7 +3,6 @@ package io.projectdiscovery.plugins.jenkins.nuclei;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
-import javax.annotation.RegEx;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
